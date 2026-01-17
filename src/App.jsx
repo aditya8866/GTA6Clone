e73a110c70ece3d691fc9d1cf889a6357c10227c
Aditya Saini <adityasaini63555@gmail.com>
2026-01-17 18:06:02 +0530

    Completed the hero section and implement the first video part

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import gsap from "gsap"
 import {ScrollTrigger} from 'gsap/all'
+import Hero from './Sections/Hero';
+import Navbar from './Sections/Navbar';
+import FirstVideo from './Sections/FirstVideo';
+import Lenis from 'lenis'
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
+
+  // useEffect(() => {
+  //   const lenis = new Lenis();
+
+  //   function raf(time) {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(raf);
+  //   }
+
+  //   requestAnimationFrame(raf);
+
+  //   return () => {
+  //     lenis.destroy();
+  //   };
+  // }, []);
+
+    
+     
+
   return (
-    <main>
-       <p className='text-pink'>asdfasdf</p> 
+    <main>  
+        
+        <Navbar/>
+        <Hero/>
+        <FirstVideo/>
+
     </main>
   )
 }
---
+++ src/Sections/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className=""> 
+      <img src="images/nav-logo.svg" alt="" className="scale-75" />
+      <img src="images/menu.svg" alt="" className="w-9" />
+    </nav>
+  );
+};
+
+export default Navbar;
